07bb821fc469a9de5ad5c9327eef1ca8450b76e9
goffart-web <kithulovalibin@gmail.com>
2024-12-26 11:26:37 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+let myName="kkkgoffart"
+console.log(typeof "goffart")
+console.log(myName.charAt())
+console.log(Math.floor(Math.random()*4+1))
+console.log(Math.floor(Math.random()*4+1))
+console.log(myName.charAt(Math.random()*6+1))
+console.log(myName.charAt(Math.random()*6 +1))
+console.log(myName.charAt(Math.random()*6 +1))
+console.log(myName.charAt(Math.random()*6 +1))
